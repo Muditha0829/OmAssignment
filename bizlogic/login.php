ef0a5abd52489e6c6c23337df9746cfb441921c4
Muditha0829 <muditha1gms@gmail.com>
2022-03-29 14:54:18 +0530

    Commit

---
+++ bizlogic/login.php
@@ -0,0 +1,23 @@
+<?php
+require 'connect.php';
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+ 
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata)){
+    $request = json_decode($postdata);
+     
+     
+    $email = $request->email;
+    $password = $request->password;
+    $sql = "INSERT INTO users (email,password) VALUES ('$email','$password')";
+    if(mysqli_query($db,$sql)){
+        http_response_code(201);
+    }
+    else{
+         http_response_code(422); 
+    }
+         
+}
+?>
